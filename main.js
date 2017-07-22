137ac869a9381962e2296ab262377edb3dc9da20
Sang Dang <sangdth@gmail.com>
2017-07-22 20:38:12 +0300

    test commit but will not push, pull instead

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const electron = require('electron')
 
-// Module to control application life. Test thu git-plus inside atom 
+// Module to control application life. 
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
